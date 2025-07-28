025aa4cd754f1242d9c2cfa5e79d6f89d882807c
paulmaxeiner <maxeiner@wustl.edu>
2025-07-27 21:36:01 -0500

    CSS update

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
   return (
 
     <div className="min-h-screen bg-white text-gray-800 dark:bg-gray-900 dark:text-white font-sans">
-      <main className="max-w-5xl mx-auto px-4">
+      <main className="mx-auto px-4 items-center">
         <Page />
       </main>
 
