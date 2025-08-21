bb57bd22a37fa1964fca860249af3a662214046f
paulmaxeiner <maxeiner@wustl.edu>
2025-08-21 08:04:52 -0400

    Pages publish test

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/portfolio/dist/',
+  base: '/portfolio/',
   plugins: [
     react(),
     tailwindcss()
